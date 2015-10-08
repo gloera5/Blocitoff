32fa98c819ab6fd123d7a503831e8177ea0fd8db
gloera5 <gloera5@live.com>
2015-10-08 00:15:11 +0000

    Item destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,18 @@
 class ItemsController < ApplicationController
   def create
-    @item = Item.new(...)
+    @item = current_user.items.build(params.require(:item).permit(:name))
     @item.save
-    redirect to: @item.user
+    redirect_to @item.user
   end
+  
+   def destroy
+     @item = Item.find(params[:id])
+     
+     if @item.destroy
+       flash[:notice] = "\"#{@item.name}\" was deleted successfully."
+       redirect_to @item.user
+     else
+       flash[:error] = "There was an error deleting the item."
+     end
+   end
 end
